27136e993878bb3c65c07f7f1acb81bcc0fd6afa
Jason S. Gold <jason.gold@careerbuilder.com>
2015-12-13 18:26:35 -0500

    added Posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151213014140) do
+ActiveRecord::Schema.define(version: 20151213231518) do
 
   create_table "applications", force: :cascade do |t|
     t.integer  "user_id"
@@ -57,6 +57,14 @@ ActiveRecord::Schema.define(version: 20151213014140) do
     t.datetime "updated_at",         null: false
     t.float    "latitude"
     t.float    "longitude"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
